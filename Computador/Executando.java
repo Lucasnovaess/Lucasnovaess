adba35b25edbc5c4fa1a59c1de6b9c3494fdde35
Lucas Novaes <155585042+Lucasnovaess@users.noreply.github.com>
2024-06-01 15:53:57 -0300

    Coding a simple model of a computer in OOP

---
+++ Computador/Executando.java
@@ -0,0 +1,17 @@
+package Exercicio.Computador;
+
+public class Executando {
+
+    public static void main(String[] args) {
+
+        Computador pc = new Computador();        
+        pc.ligar();
+        pc.conecMouse();
+        pc.conecTeclado();
+        pc.clicar();
+        pc.digitar();
+        pc.trabalhar();
+        pc.sobrePc();
+    }
+
+}
